b445317d4c98c0be6ea5d333eb325e455af4dc02
逄杭之 <alina@panghangzhideMacBook-Air.local>
2017-10-20 20:49:28 +0800

    finish creating entities

---
+++ OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/entity/EmployeeEntity.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import util.enumeration.EmployeeAccessRightEnum;
+
+/**
+ *
+ * @author alina
+ */
+@Entity
+@XmlRootElement
+@XmlType(name = "employeeEntity", propOrder = {
+    "id",
+    "firstName",
+    "lastName",
+    "identificationNumber",
+    "username",
+    "password",
+    "accessRight"
+})
+public class EmployeeEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 32, nullable = false)
+    private String firstName;
+    @Column(length = 32, nullable = false)
+    private String lastName;
+    @Column(length = 9, nullable = false, unique = true)
+    private String identificationNumber;
+    @Column(length = 32, nullable = false, unique = true)
+    private String username;
+    @Size(min = 6)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private EmployeeAccessRightEnum accessRight;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(Long id, String firstName, String lastName, String username, String password, EmployeeAccessRightEnum accessRight) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.accessRight = accessRight;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the accessRight
+     */
+    public EmployeeAccessRightEnum getAccessRight() {
+        return accessRight;
+    }
+
+    /**
+     * @param accessRight the accessRight to set
+     */
+    public void setAccessRight(EmployeeAccessRightEnum accessRight) {
+        this.accessRight = accessRight;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof EmployeeEntity)) {
+            return false;
+        }
+        EmployeeEntity other = (EmployeeEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.EmployeeEntity[ id=" + id + " ]";
+    }
+
+}
