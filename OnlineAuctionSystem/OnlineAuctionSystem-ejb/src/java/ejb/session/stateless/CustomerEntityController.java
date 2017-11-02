95322043058d35993c3a696f325588dcc98142d2
Pang Hangzhi <hangzhipang@gmail.com>
2017-11-02 16:23:26 +0800

    modify customerController

--- OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/CustomerEntityController.java
+++ OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/CustomerEntityController.java
@@ -5,11 +5,24 @@
  */
 package ejb.session.stateless;
 
+import entity.CustomerEntity;
+import entity.CustomerEntity;
+import entity.CustomerEntity;
+import java.util.List;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import util.exception.CustomerAlreadyExistException;
+import util.exception.DuplicateException;
+import util.exception.GeneralException;
+import util.exception.IncorrectPasswordException;
+import util.exception.CustomerNotFoundException;
+import util.exception.CustomerNotFoundException;
 
 /**
  *
@@ -23,11 +36,127 @@ public class CustomerEntityController implements CustomerEntityControllerRemote,
     @PersistenceContext(unitName = "OnlineAuctionSystem-ejbPU")
     private EntityManager em;
 
-    public void persist(Object object) {
-        em.persist(object);
+    @Override
+    public CustomerEntity createNewCustomerEntity(CustomerEntity customer) throws CustomerAlreadyExistException, GeneralException {
+        try {
+            em.persist(customer);
+            em.flush();
+            em.refresh(customer);
+
+            return customer;
+        } catch (PersistenceException ex) {
+            if (ex.getCause() != null
+                    && ex.getCause().getCause() != null
+                    && ex.getCause().getCause().getClass().getSimpleName().equals("MySQLIntegrityConstraintViolationException")) {
+                throw new CustomerAlreadyExistException("Customer with same identification number already exist!");
+            } else {
+                throw new GeneralException("An unexpected error has occurred: " + ex.getMessage());
+            }
+        }
+    }
+    
+        @Override
+    public CustomerEntity customerLogin(String username, String password) throws CustomerNotFoundException, IncorrectPasswordException, DuplicateException {
+        CustomerEntity customer = retrieveCustomerByUsername(username);
+        if (customer.getPassword().equals(password)) {
+            return customer;
+        } else {
+            throw new IncorrectPasswordException("Incorrect Password!");
+        }
+    }
+    
+        @Override
+    public CustomerEntity retrieveCustomerById(Long id) throws CustomerNotFoundException, GeneralException {
+        // retrieve the customer
+        CustomerEntity customer = em.find(CustomerEntity.class, id);
+
+        // check and throw exception
+        if (customer == null) {
+            throw new CustomerNotFoundException("Customer with id = " + id + " does not exist!");
+        } else {
+            return customer;
+        }
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-    
+    @Override
+    public CustomerEntity retrieveCustomerByIdentificationNumber(String number) throws CustomerNotFoundException {
+        // retrieve customer
+        Query query = em.createQuery("SELECT s FROM CustomerEntity s WHERE s.identificationNumber = :num");
+        query.setParameter("num", number);
+
+        try {
+            return (CustomerEntity) query.getSingleResult();
+        } catch (NoResultException ex) {
+            throw new CustomerNotFoundException("Customer with identification number = " + number + " is not found!");
+        } 
+    }
+
+    @Override
+    public CustomerEntity retrieveCustomerByUsername(String username) throws CustomerNotFoundException {
+        // retrieve customer
+        Query query = em.createQuery("SELECT s FROM CustomerEntity s WHERE s.username = :username");
+        query.setParameter("username", username);
+
+        try {
+            return (CustomerEntity) query.getSingleResult();
+        } catch (NoResultException ex) {
+            throw new CustomerNotFoundException("Customer with username = " + username + " is not found!");
+        } 
+    }
+
+    @Override
+    public CustomerEntity changePassword(String currentPw, String newPw, Long id) throws IncorrectPasswordException, CustomerNotFoundException, GeneralException {
+        CustomerEntity customer = retrieveCustomerById(id);
+
+        if (customer.getPassword().equals(currentPw)) {
+            customer.setPassword(newPw);
+            em.flush();
+            em.refresh(customer);
+
+            return customer;
+        } else {
+            throw new IncorrectPasswordException("You must insert correct old password to change your new password!");
+        }
+    }
+
+    // manager can only update the firstname, lastname, accessRight, identificationNumber. others are cretical information, and must be hidden from the manager.
+    @Override
+    public CustomerEntity updateCustomer(CustomerEntity newCustomer) throws CustomerNotFoundException, GeneralException {
+        CustomerEntity oldCustomer = retrieveCustomerById(newCustomer.getId());
+
+        oldCustomer.setFirstName(newCustomer.getFirstName());
+        // TO DO: add in all other attributes setter and getters
+
+        try {
+            em.flush();
+            em.refresh(oldCustomer);
+        } catch (PersistenceException ex) {
+            if (ex.getCause() != null
+                    && ex.getCause().getCause() != null
+                    && ex.getCause().getCause().getClass().getSimpleName().equals("MySQLIntegrityConstraintViolationException")) {
+                throw new GeneralException("Customer with same identification number already exist!");
+            } else {
+                throw new GeneralException("An unexpected error has occurred: " + ex.getMessage());
+            }
+        }
+
+        return oldCustomer;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) throws CustomerNotFoundException, GeneralException {
+        CustomerEntity customer = retrieveCustomerById(id);
+
+        em.remove(customer);
+    }
+
+    @Override
+    public List<CustomerEntity> viewAllCustomer() throws GeneralException {
+        Query query = em.createQuery("SELECT s FROM CustomerEntity s");
+        try {
+            return (List<CustomerEntity>) query.getResultList();
+        } catch (Exception ex) {
+            throw new GeneralException("An unexpected exception happens: " + ex.getMessage());
+        }
+    }
 }
