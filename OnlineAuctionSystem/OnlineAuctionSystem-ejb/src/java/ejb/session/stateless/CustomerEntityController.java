6381de0987ebd49c7dca0ee77fa0b85df86717a8
GuanqiW <wgq19980409@gmail.com>
2017-11-02 16:30:55 +0800

    new methods in customer controller

--- OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/CustomerEntityController.java
+++ OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/CustomerEntityController.java
@@ -5,11 +5,17 @@
  */
 package ejb.session.stateless;
 
+import entity.CustomerEntity;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import util.exception.CustomerAlreadyExistsException;
+import util.exception.CustomerNotFoundException;
+import util.exception.CustomerPinChangeException;
+import util.exception.GeneralException;
 
 /**
  *
@@ -23,10 +29,66 @@ public class CustomerEntityController implements CustomerEntityControllerRemote,
     @PersistenceContext(unitName = "OnlineAuctionSystem-ejbPU")
     private EntityManager em;
 
-    public void persist(Object object) {
-        em.persist(object);
+    @Override
+    public CustomerEntity persistCustomerEntity (CustomerEntity customer) throws CustomerAlreadyExistsException, GeneralException
+    {
+        try
+        {
+            em.persist(customer);
+            em.flush();
+            em.refresh(customer);
+            
+            return customer;
+        }
+        catch(PersistenceException ex)
+        {
+            if(ex.getCause()!=null
+                    &&ex.getCause().getCause()!=null
+                    &&ex.getCause().getCause().getClass().getSimpleName().equals("MySQLIntegrityConstraintViolationException"))
+                {
+                throw new CustomerAlreadyExistsException("Customer with same identification number already exist");
+            }
+            else
+            {
+                throw new GeneralException("An unexpected error has occurred: " + ex.getMessage());
+            }
+        }
+            
+        
     }
 
+    
+    @Override
+    public CustomerEntity retrieveCustomerByEmail(String email) throws CustomerNotFoundException
+    {
+        try
+        {
+            CustomerEntity customer = em.find(CustomerEntity.class, email);//usage of jpa queru language
+            
+            return customer;
+        }
+        catch(PersistenceException ex)
+        {
+            throw new CustomerNotFoundException("This email "+email+" haven't registered an account!");
+        }
+        
+    }
+    
+    public void changePassword(CustomerEntity customer, String oldpassword, String newpassword)throws CustomerPinChangeException
+    {
+        if(oldpassword.equals(customer.getPassword()))
+        {
+            customer.setPassword(newpassword);
+        }
+        else
+        {
+            throw new CustomerPinChangeException("Current password is invalid!");
+        }
+        
+    }
+    
+    
+    
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
     
